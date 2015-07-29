520d827a4f9119f97b61f03fd32d5375d73bf8c1
Nicholas Lui <nicholasluimy@gmail.com>
2015-07-29 11:31:57 +0800

    logic part 1

--- orbital-timekeeper/orbital-timekeeper.js
+++ orbital-timekeeper/orbital-timekeeper.js
@@ -1,31 +1,54 @@
+AccountLogged = new Meteor.Collection('accountlogged');
+AccountUnlogged = new Meteor.Collection('accountunlogged');
+Instance = new Meteor.Collection('instance');
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.header.helpers({
-    'loggedInStatus' : function () {
-        loggedIn = 0;
-        if(loggedIn==0){
-            return "{{>loginButton}}";
-        }
-        else{
-            return "{{>loggedIn}}";
-        }
 
 
-    }
+
+  Template.landing.events({
+      'click .begin' : function(){
+          Router.go('startup');
+      }
   });
 
+
+    Template.startup.events({
+        'click .next' : function(){
+            var url = $('#input_url').val();
+            var email = $('#input_email').val();
+
+            var accId = AccountUnlogged.insert({
+               name: '',
+                url: url,
+                email: email
+            });
+
+            Instance.insert({
+                name: '',
+                unloggedIds: [accId]
+            }, function(error, results){
+                Router.go('timetable', {_id: results});
+            });
+
+
+        }
+    });
+
+    // STUCK HERE
+    Template.timetable.helpers({
+        'testhelper' : function(){
+            var testlst = this;
+            console.log(this);
+            return this;
+        }
+    });
   //Template.loggedIn.helpers({
   //    return "<h5>Welcome"+loginName+"</h5>"
   //})
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
 }
 
 if (Meteor.isServer) {
@@ -33,7 +56,12 @@ if (Meteor.isServer) {
 
     // code to run on server at startup
   });
+
+
+
+
 }
 
 //database
 //membersList = new.Mongo.Collection('members');
+
